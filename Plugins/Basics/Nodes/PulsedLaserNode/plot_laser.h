8ba009aef75baa5ec2b825b2d7cc2d8dca413d5e
Carlos Pineiro <carletto.moreno00@gmail.com>
2025-07-03 10:06:37 +0000

    Initial commit starting fresh

---
+++ Plugins/Basics/Nodes/PulsedLaserNode/plot_laser.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <QMainWindow>
+#include <QtWidgets>
+#include <QtCharts/QChartView>
+#include <QtCharts/QLineSeries>
+
+#include "../../Lib/header/laser.h"
+
+
+class Laser_Analysis_Window : public QMainWindow
+{
+public:
+    explicit Laser_Analysis_Window(std::shared_ptr<QC::Laser> laser);
+
+
+private:
+    std::shared_ptr<QC::Laser> laser;
+    std::string direction;
+
+private:
+    QLineEdit* time_min;
+    QLineEdit* time_max;
+    QChart* chart;
+    void plot();
+};
+
